21190498d39c744d8cb4ad0bf8ab17f0086af1f2
vodaiphuoc <phuocvodn98@gmail.com>
2025-05-31 22:04:18 +0700

    refactoring model

--- going_modular/model/backbone/mifr.py
+++ going_modular/model/backbone/mifr.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-from .irse import iResNet, BasicBlock, Bottleneck
+from .irse import iResNet, BasicBlock, Bottleneck, conv1x1, conv3x3
 
 # Đặt seed toàn cục
 seed = 42
@@ -92,14 +92,81 @@ class AttentionModule(nn.Module):
 
 
 # Backbone trích xuât featuremap
-class MIResNet(iResNet):
-    def __init__(self, block, layers, **kwargs):
-        super(MIResNet, self).__init__(block, layers, **kwargs)
+class MIResNet(torch.nn.Module):
+    def __init__(self, block, layers, zero_init_residual=False, norm_layer=None):
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(64)
+        self.prelu = nn.PReLU()
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=2, norm_layer=norm_layer)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, norm_layer=norm_layer)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, norm_layer=norm_layer)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, norm_layer=norm_layer)
+        
         self.spectacles_fsm = AttentionModule()
         self.facial_hair_fsm = AttentionModule()
         self.emotion_fsm = AttentionModule()
         self.pose_fsm = AttentionModule()
         self.gender_fsm = AttentionModule()
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')  # Default 'relu'
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.PReLU):  # Initialize PReLU parameters separately
+                nn.init.constant_(m.weight, 0.25)
+
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, norm_layer=None):
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        downsample = None
+        if stride != 1 and self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.MaxPool2d(kernel_size=3, stride=stride, padding=1),
+                conv1x1(self.inplanes, planes * block.expansion),
+                norm_layer(planes * block.expansion),
+            )
+        elif self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion),
+                norm_layer(planes * block.expansion),
+            )
+        elif stride != 1:
+            downsample = nn.MaxPool2d(kernel_size=3, stride=stride, padding=1)
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, norm_layer,
+                            start_block=True))
+        self.inplanes = planes * block.expansion
+        exclude_bn0 = True
+        for _ in range(1, (blocks-1)):
+            layers.append(block(self.inplanes, planes, norm_layer=norm_layer,
+                                exclude_bn0=exclude_bn0))
+            exclude_bn0 = False
+
+        layers.append(block(self.inplanes, planes, norm_layer=norm_layer, end_block=True, exclude_bn0=exclude_bn0))
+
+        return nn.Sequential(*layers)
+
+
 
     def forward(self, x):
         x = self.conv1(x)
